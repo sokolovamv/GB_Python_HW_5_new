64774e23f5f66fa8112029ac4d6469e31cba551a
sokolovamv <109550333+sokolovamv@users.noreply.github.com>
2022-10-27 21:04:11 +0300

    Update func_files.py

--- lesson1/func_files.py
+++ lesson1/func_files.py
@@ -1,12 +1,14 @@
-
+# запись введенного текста
 def write_file_w(path):
     with open(path, 'w', encoding="utf-8") as file:
         file.write(input("Введите текст: "))
-
+        
+# чтение
 def read_file_r(path):
     with open(path,'r', encoding="utf-8") as file:
         return file.read()
-
+    
+# запись текста
 def write_file(path, text):
     with open(path, 'w', encoding="utf-8") as file:
         file.write(text)
